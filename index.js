3ab1555f42bd2ebf2ec7682d6a3a000fa800cb18
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-05-14 12:13:10 +0500

    added

---
+++ index.js
@@ -0,0 +1,125 @@
+// 1-chi misol
+
+
+
+// let a = prompt("What is your age?");
+// if (a > 18) {
+//     alert("Siz voyaga yetgansiz")
+//
+// }else {
+//     alert("Siz voyaga yetmagansiz")
+// }
+
+// 2-chi misol
+
+
+
+// let a = prompt("son kiriting");
+// if (a > 0) {
+//     alert("bu musbat son")
+//     }    else if (a == 0) {
+//     alert("0 son kiritdinggiz")
+// }else{
+//     alert("bu manfiy son")
+// }
+
+// 3-misol
+
+
+
+// let a = prompt("parolkiriting")
+// let b = "manfaat123"
+// if (a === b) {
+//     alert("parol to'g'ri")
+// }else {
+//     alert("parol noto'g'ri")
+// }
+
+// 4-misol
+
+
+// let a = prompt('baho kiriting')
+// if (90 <= a) {
+//     alert("Alo")
+// }else if (70 <= a && a <= 89) {
+//     alert("yaxshi")
+// }else if (50 <= a && a <= 69) {
+//     alert("qoniqarli")
+// }else if (a <= 50) {
+//     alert("yomon")
+// }
+
+// 5-misol
+
+
+// let a = prompt("Hafta kunini kiriting")
+// if (a === "shanba" || a === "yakshanba") {
+//     alert("dam olish kuni")
+// }else if (
+//     a === "dushanba" ||
+//     a === "seshanba" ||
+//     a === "chorshanba" ||
+//     a === "payshanba " ||
+//     a === "juma"
+// ) {
+//     alert("ish kuni")
+// }else{
+//     alert("bunaqa hafta kuni yuq")
+// }
+
+// 6-misol
+
+
+// let a = prompt("haroratni kiriting")
+// if (a >= 30) {
+//     alert("juda issiq")
+// }else if (a >= 15 && a <= 30) {
+//     alert("ob-havo yaxshi")
+// }else if (a >= 0 && a <= 15) {
+//     alert("salqin")
+// }else if (a <= 0){
+//     alert("sovuq")
+// }
+
+// 7-misol
+
+
+// let a = prompt("Birinchi sonni kiriting")
+// let b = prompt("Ikkinchi sonni kiriting")
+// let c = prompt("Uchinchi sonni kiriting")
+// if (a >= b && a > c) {
+//     alert(a + " Katta son")
+// }else if (b >= a && b >= c) {
+//     alert(b + " Katta son")
+// }else if (c > a && c >= b) {
+//     alert(c + " Katta son")
+// }else{
+//     alert(" hammasi teng")
+// }
+
+// 8-misol
+
+
+// let a = prompt("Birinchi tomonini kiriting kiriting")
+// let b = prompt("Ikkinchi tomonini kiriting kiriting")
+// let c = prompt("Uchinchi tomonini kiriting kiriting")
+// if (a === b && b === c) {
+//     alert("Teng tomonli uchburchak")
+// }else if (a === b || b === c || a === c) {
+//     alert("Teng yonli uchburchak")
+// }else {
+//     alert("Turli tomonli uchburchak")
+// }
+
+
+
+// 9-misol
+
+
+// let a = prompt("Telefon raqamni kiriting")
+// let b = +998
+// if (a * 1 === b) {
+//     alert("Uzbekiston raqami")
+// }else{
+//     alert("chet el raqami")
+// }

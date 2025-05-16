028fe97c59e44766dbc7cb5ab1a76f11a973e316
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-05-16 14:19:22 +0500

    added2

--- index.js
+++ index.js
@@ -123,3 +123,14 @@
 // }else{
 //     alert("chet el raqami")
 // }
+
+
+// let a =0
+// for (let i = 0; i < 10; i++){
+//     if (i % 2===1  ){
+//
+//        a += i
+//
+//     }
+// }
+// console.log(a)
---
+++ new.js
@@ -0,0 +1,72 @@
+// for (let a= 10; a > -10; a--){
+//     console.log("hello")
+//
+//     for (let i = 0 ; i < 5; i++)
+//         console.log("world")
+// }
+
+// 1-misol
+
+
+// let a = 5
+// let b = 10
+// console.log("B")
+// for (a; a > 0; a -- ) {
+//     console.log(a)
+// }
+// console.log("B")
+// for (b; b > 0; b -- ) {
+//     console.log(b)
+// }
+
+// 2-misol
+
+
+// let a = 500
+// for (let i = 1; i <= 10; i++ ) {
+//     for (let j = 1; j <= 1; j++ ) {
+//         console.log(j*i + " kg konfet")
+//     }
+//     console.log(a * i + " so'm")
+// }
+
+// 3-misol
+
+
+// let sum = 100
+// for (let i = 0.1; i <= 1.0; i+= 0.1) {
+//     console.log(`${i.toFixed(1)} kg = ${(sum * i ).toFixed(0)} so'm`)
+// }
+
+// 4-misol
+
+// let a = 3
+// let b = 7
+// let c = 0
+// for (let i = a; i <= b; i++ ) {
+//     c += i
+// }
+// console.log(`Yig'indisi: ${c}`)
+
+// 5-misol
+
+// let b = 1
+// let  first = 2
+// let second = 5
+// for (let a = first; a <= second; a++) {
+//   b *=a
+// }
+// console.log(b)
+
+// 6-misol
+
+
+// for (let a = 1; a <= 100; a++) {
+//     if (a % 3 === 0) {
+//         console.log("Fizz")
+//     }else if (a % 5 === 0) {
+//         console.log("Buzz")
+//     }else{
+//         console.log("FizzBuzz")
+//     }
+// }
